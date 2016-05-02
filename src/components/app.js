d299b9719e8d347dc9bf3a4023a2482df09e4808
klintg <theklint1@gmail.com>
2016-05-02 19:58:03 +0300

    higher order function

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component } from 'react';
+import Header from './Header'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+
+        {this.props.children}
+      </div>
+    );
+  }
+}
